1b1d7cd24e6d809c6748ab498861c4e3aab4b23f
Ricardo Martinez <remo87@gmail.com>
2018-12-09 21:33:36 -0500

    add authors, and add books to answers

--- src/AuthorQuiz.js
+++ src/AuthorQuiz.js
@@ -3,14 +3,57 @@ import logo from './logo.svg';
 import './App.css';
 import './bootstrap.min.css';
 
-class AuthorQuiz extends Component {
-  render() {
+function Hero(params) {
+  return (<div className="row">
+    <div className="jumbotron col-10 offset-1">
+      <h1>Author Quiz</h1>
+      <p>Select the book written by the author shown</p>
+    </div>
+  </div>);
+}
+
+function Book({title}) {
+  return (
+    <div className="answer">
+      <h4>{title}</h4>
+    </div>
+  );
+}
+
+function Turn({author, books}) {
+  return(<div className="row turn" style={{backgroundColor:"white"}}>
+    <div className="col-4 offset-1">
+      <img src={author.imageUrl} className="authorimage" alt="Author" />
+    </div>
+    <div className="col-6">
+      {books.map((title) => <Book title={title} key={title} />)}
+    </div>
+  </div>);
+}
+
+function Continue(params) {
+  return (<div />);
+}
+
+function Footer(params) {
+  return(<div id="footer" className="row">
+    <div className="col-12">
+      <p className="text-muted credit">All images from <a href="https://en.wikipedia.org/wiki/Main_Page">wikipedia</a></p>
+    </div>
+  </div>);
+}
+
+function AuthorQuiz({turnData}) {
+  
     return (
-      <div className="App">
-        AuthorQuiz
+      <div className="container-fluid">
+        <Hero />
+        <Turn {...turnData}/>
+        <Continue />
+        <Footer />
       </div>
     );
-  }
+  
 }
 
 export default AuthorQuiz;
